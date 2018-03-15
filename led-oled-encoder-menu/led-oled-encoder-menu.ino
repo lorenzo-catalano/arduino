a6f92ec35b19a995c9e25b9ab080d98dc09b1f07
lorenzo_catalano <lorux87@gmail.com>
2018-03-15 22:40:17 +0100

    init

--- led-oled-encoder-menu/led-oled-encoder-menu.ino
+++ led-oled-encoder-menu/led-oled-encoder-menu.ino
@@ -31,7 +31,7 @@ String menu[] = {
   };
 char menuManual[] = {
   '.',
-  'R',
+  'R', 
   'G',
   'B'
   };
@@ -40,20 +40,20 @@ char menuManual[] = {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(1);
-    display.setTextColor(WHITE);
+  display.setTextColor(WHITE);
   display.setTextWrap(false);
-  
-   pinMode (pin_A,INPUT);
-   pinMode (pin_B,INPUT);
-   
-   Serial.begin (9600);
-   currentTime = millis();
+  pinMode (pin_A,INPUT);
+  pinMode (pin_B,INPUT);
+  Serial.begin (9600);
+  currentTime = millis();
   loopTime = currentTime; 
   pinMode(pin_button, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pin_button), select, RISING);
+  //attachInterrupt(digitalPinToInterrupt(pin_button), click, RISING);
  } 
-int selected = 2;
+ 
+int selected = 0;
 int selectedColor=0;
+boolean editing=false;
 
 int menutype=0;
 int current = 0;
@@ -62,66 +62,113 @@ int start = 0;
 int window = 4;
 int r=30,g=70,b=200;
 
-void select() {
-  int oldmenutype=menutype;
-  selected=current;
+void click() {
+  boolean menuchanged = false;
   switch(menutype){
-    case 0:      
-      if(selected==2)menutype = 1;
+    case 0:
+      if(current==2){
+        menutype = 1;
+        menuchanged=true;
+        }
+      if(menuchanged){
+        current=0;
+        selected=0;
+        editing=false;
+      }else{
+        selected=current;
+      }
       break;
     case 1:
-      selectedColor=current;
-      if(selected==0)menutype = 0;
+      if(current==0){
+        menutype = 0;
+        menuchanged=true;
+        }
+      if(menuchanged){
+        current=0;
+        selected=0;
+        editing=false;
+      }else{
+        selected=current;
+        editing=!editing;
+        if(editing){
+          selectedColor=current;
+        }else{
+          selectedColor=0;
+        }
+        
+      }
       break;
   }
-  if(menutype!=oldmenutype){current=0;selected=0;};
   redraw=1;
 }
-int barwidth= display.width()-40;
 
+int barwidth= display.width()-18;
+int maxc = 255;
 void loop()  {
+
+ 
 
   currentTime = millis();
   if(currentTime >= (loopTime)){
+    if (digitalRead(pin_button) != HIGH) {
+      click();
+      delay(200);
+    }
+    loopTime = currentTime+200;
+  }
     
-    encoder_A = digitalRead(pin_A);    // Read encoder pins
-    encoder_B = digitalRead(pin_B); 
-    if((!encoder_A) && (encoder_A_prev)){
-      if(encoder_B) {
+  encoder_A = digitalRead(pin_A);    // Read encoder pins
+  encoder_B = digitalRead(pin_B);
+  if((!encoder_A) && (encoder_A_prev)){
+    
+    if(encoder_B) {
+      if(editing){
+        switch(selectedColor){
+          case 1:
+            if(r>0)r-=5;
+            break;
+          case 2:
+            if(g>0)g-=5;
+            break;
+          case 3:
+            if(b>0)b-=5;
+            break;
+          default:break;
+          }
+      }else{
         if(current>0)current--;
-//        if(current>(window-1)){
-//          start = (window-1);
-//        }else if(current>0){
-//          start = current-1;
-//        }else{
-//          start=0;
-//        }
+      }
+    }else{
+      if(editing){
+        switch(selectedColor){
+          case 1:
+            if(r<maxc)r+=5;
+            break;
+          case 2:
+            if(g<maxc)g+=5;
+            break;
+          case 3:
+            if(b<maxc)b+=5;
+            break;
+          default:break;
+          }
       }else{
         if(current<entries-1)current++;
-//        if(current>2 && current<(entries-1)){
-//          start = current-2;
-//        }else if(current>=(entries-1)){
-//          start = current-(window-1);
-//        }else{
-//          start=0;
-//        }
       }
-      start=0;
-      redraw=1;
+      
     }
-    encoder_A_prev = encoder_A;
-    loopTime = currentTime;
+    start=0;
+    redraw=1;
   }
+  encoder_A_prev = encoder_A;
+    
+  
   if(redraw){
     display.clearDisplay();
-    display.setCursor(64,0);
-    display.print(menutype);
-    display.print(" ");
-    display.print(selected);
-    display.print(" ");
-    display.print(current);
+    
     if(menutype==1){
       display.setCursor(0,0);
+      
       for(int i=start;i<(start+window);i++){
         if(current == i){
           display.print(">");
@@ -129,28 +176,34 @@ void loop()  {
           display.print(" ");
         }
         display.print(menuManual[i]);
-        if(selectedColor==i)display.print("*");
+        if(i>0 && selectedColor==i)display.print("*");
           
         int value=0;
         switch(menuManual[i]){
           case 'R':
             value = map(r, 0, 255, 0, barwidth);
-            display.fillRect(20,9+8*(i-1), value, 6, WHITE);
-            display.drawRect(20,9+8*(i-1), display.width()-20, 6, WHITE);
+            display.fillRect(18,9+8*(i-1), value, 6, WHITE);
+            display.drawRect(18,9+8*(i-1), barwidth, 6, WHITE);
             break;
           case 'G':
             value = map(g, 0, 255, 0, barwidth);
-            display.fillRect(20,9+8*(i-1), value, 6, WHITE);
-            display.drawRect(20,9+8*(i-1), display.width()-20, 6, WHITE);
+            display.fillRect(18,9+8*(i-1), value, 6, WHITE);
+            display.drawRect(18,9+8*(i-1), barwidth, 6, WHITE);
             break;
           case 'B':
             value = map(b, 0, 255, 0, barwidth);
-            display.fillRect(20,9+8*(i-1), value, 6, WHITE);
-            display.drawRect(20,9+8*(i-1), display.width()-20, 6, WHITE);
+            display.fillRect(18,9+8*(i-1), value, 6, WHITE);
+            display.drawRect(18,9+8*(i-1), barwidth, 6, WHITE);
             break;
         }
         display.println("");
       }
+      display.setCursor(63,0);
+      display.print(r);
+      display.print(" ");
+      display.print(g);
+      display.print(" ");
+      display.print(b);
       display.display();       
     }else{
       display.setCursor(0,0);
